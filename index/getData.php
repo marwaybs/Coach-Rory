a7bf12967ddb6b5c6bde6ff0cb6f7cf8d6585d6f
marwaybs <bsmarway@gmail.com>
2017-06-15 09:32:21 -0400

    Fixed permissions on server

--- index/getData.php
+++ index/getData.php
@@ -9,6 +9,6 @@ if($_POST['action'] == "send") {
   $command = "sudo python2 sentiment.py \"" .$feelings ."\"";
   // echo ($command);
   $output = (shell_exec($command));
-  echo("monkey");
+  echo($output);
 }
 ?>
